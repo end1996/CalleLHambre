8932e6a3e3d315e214a20e7b832d32dd8697427f
Enmanuel <end199615@gmail.com>
2025-07-04 09:57:50 -0500

    Añadir titulo de categorias y unificacion de categorias con productos

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,35 @@
 import { Card, Col, Row } from "react-bootstrap";
-import { products } from "@/data/products";
-import {categories} from "@/data/categories";
+import { categories } from "@/data/categories";
 
 function Menu({}) {
   return (
     <>
-    <div>
-      {categories.map((category)=> (
-        <h1>{category.name}</h1>
+      {categories.map((category) => (
+        <div key={category.id_category} className="mb-4 mt-4">
+          <h3>{(category.name).toUpperCase()}</h3>
+          <Row>
+            {category.products.map((product) => (
+              <Col xs={12} sm={6} md={6} key={product.id} className="p-3">
+                <Card className="d-flex flex-row">
+                  <Card.Img
+                    src={product.img}
+                    style={{ flex: "0 0 20%", minWidth: "100px" }}
+                  />
+                  <Card.Body className="d-flex flex-column flex-grow-1">
+                    <div className="pb-4">
+                      <Card.Title>{product.title}</Card.Title>
+                      <Card.Subtitle>{product.subtitle}</Card.Subtitle>
+                    </div>
+                    <Card.Footer className="mt-auto">
+                      {product.price}
+                    </Card.Footer>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
       ))}
-    </div>
-      <Row>
-        {products.map((product) => (
-          <Col xs={12} sm={6} md={6} key={product.id} className="p-3">
-            <Card className="d-flex flex-row">
-              <Card.Img
-                src={product.img}
-                style={{ flex: "0 0 20%", minWidth: "100px" }}
-              />
-              <Card.Body className="d-flex flex-column flex-grow-1">
-                <div className="pb-4">
-                  <Card.Title>{product.title}</Card.Title>
-                  <Card.Subtitle>{product.subtitle}</Card.Subtitle>
-                </div>
-                <Card.Footer className="mt-auto">{product.price}</Card.Footer>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
     </>
   );
 }
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,22 +1,79 @@
 export const categories = [
   {
-    id: 1,
-    name: 'Promos'
+    id_category: 1,
+    name: "Promos",
+    products: [
+      {
+        id: 101,
+        title: "Combo 6.0",
+        subtitle:
+          "Hamburguesa Doble carne de 110 gr + Doble Queso cheddar + Cebolla caramelizada con tocino + Tocino + Onion ring + Arugula + Papas fritas + Gaseosa de 1lt.",
+        price: "S/. 29,90",
+        img: "https://placeholdit.com/100x100/dddddd/999999",
+      },
+      {
+        id: 102,
+        title: "Combo The Goldogs",
+        subtitle: "Pepito The Goldogs + Papas fritas + Gaseosa de 1lt.",
+        price: "S/. 44,90",
+        img: "https://placeholdit.com/100x100/cccccc/666666",
+      },
+      {
+        id: 103,
+        title: "Combo Cachapa",
+        subtitle: "3x2 en Cachapas con queso de mano",
+        price: "S/. 44,00",
+        img: "https://placeholdit.com/100x100/cccccc/999999",
+      },
+      {
+        id: 104,
+        title: "Tote Bag Calle Lhambre",
+        subtitle: "Subtitulo",
+        price: "S/. 29,90",
+        img: "https://placeholdit.com/100x100/cccccc/666666",
+      },
+    ],
   },
   {
-    id: 2,
-    name: 'Entradas'
+    id_category: 2,
+    name: "Entradas",
+    products: [
+      {
+        id: 201,
+        title: "TEQUEÑOS DE QUESO 5 UNID",
+        subtitle:
+          "5 unidades de tequeños con queso.",
+        price: "S/. 15,50",
+        img: "https://placeholdit.com/100x100/dddddd/999999",
+      },
+      {
+        id: 202,
+        title: "TEQUEÑOS DE QUESO 10 UNID",
+        subtitle: "10 unidades de tequeños con queso",
+        price: "S/. 24,00",
+        img: "https://placeholdit.com/100x100/cccccc/666666",
+      },
+    ],
   },
-    {
-    id: 3,
-    name: 'Smash Burger'
-  },
-    {
-    id: 4,
-    name: 'Hamburguesas'
-  },
-    {
-    id: 5,
-    name: 'Perros calientes'
+  {
+    id_category: 3,
+    name: "Smash Burger",
+    products: [
+      {
+        id: 301,
+        title: "Smash doble",
+        subtitle:
+          "Doble carne artesanal de 100gr, doble queso cheddar, tocineta",
+        price: "S/. 16,00",
+        img: "https://placeholdit.com/100x100/dddddd/999999",
+      },
+      {
+        id: 302,
+        title: "PEPI SMASH",
+        subtitle: "Carne artesanal de 100gr, queso cheddar, pepinillo, doble tocino",
+        price: "S/. 16,00",
+        img: "https://placeholdit.com/100x100/cccccc/666666",
+      },
+    ],
   },
 ];
